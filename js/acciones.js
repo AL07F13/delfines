e50c6261f0dea0e7c03e30e1ae40abaf20283a5a
AL07F13 <ceronherrey@gmail.com>
2015-06-24 13:16:19 -0500

    uno
    delfines

---
+++ js/acciones.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#mypanel').on('tap',function (){
+		navigator.vibrate(1000);
+		});
+	$('#delfines').on('tap',function (){
+		navigator.vibrate(1000);
+		});
+	$('#especies').on('tap',function (){
+		navigator.vibrate(1000);
+		});
+	$('#alimentacion').on('tap',function (){
+		navigator.vibrate(1000);
+		});
+	$('#respiracion').on('tap',function (){
+		navigator.vibrate(1000);
+		});
+	$('#reproduccion').on('tap',function (){
+		navigator.vibrate(1000);
+		});
+	
+}); 
+});
+
